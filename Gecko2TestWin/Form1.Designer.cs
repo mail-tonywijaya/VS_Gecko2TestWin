8da5562fb9f31695e5d0917d6af743bccf19373f
mail-tonywijaya <89070256+mail-tonywijaya@users.noreply.github.com>
2021-10-06 17:52:27 +0700

    Initial commit

---
+++ Gecko2TestWin/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Gecko2TestWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.ConsoleMessageEventReceivesConsoleLogCalls = true;
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "Firefox Test";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Gecko2TestWin/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Gecko;
+using System;
+using System.Windows.Forms;
+
+namespace Gecko2TestWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Xpcom.Initialize("Firefox");
+            geckoWebBrowser1.Navigate("https://stmikpontianak.net/011100862/webview_lanjutan.html");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.AddMessageEventListener(
+                "ShowFontDialog",
+                (string nothing) => ShowFontDialog()
+                );
+
+            geckoWebBrowser1.AddMessageEventListener(
+                "ShowMessageBox",
+                (string message) => ShowMessageBox(message)
+                );
+        }
+
+        private void ShowFontDialog()
+        {
+            fontDialog1.ShowDialog();
+        }
+
+        private void ShowMessageBox(string message)
+        {
+            MessageBox.Show(message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
